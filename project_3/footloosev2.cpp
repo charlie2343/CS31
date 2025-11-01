36618ffff603ac5a5cf5778a598dd273bc97bd6c
charlie2343 <117702726+charlie2343@users.noreply.github.com>
2025-10-31 23:06:40 -0700

    sexy working isSyntacticallyCorrect Function

--- project_3/footloosev2.cpp
+++ project_3/footloosev2.cpp
@@ -5,18 +5,20 @@ using namespace std;
 bool isSyntacticallyCorrect(string dance);
 int translateDance(string dance, string &instructions, int &badBeat);
 bool isValidDir(char c);
+bool isValidBeat(string s);
 
-int main(){ 
-for (;;)
+int main()
+{
+    for (;;)
     {
         cout << "Enter dance: ";
         string d;
         getline(cin, d);
         if (d == "quit")
             break;
-        cout << "isSyntacticallyCorrect returns ";
+        cout << "isSyntacticallyCorrect returns " << endl;
         string inst = "ad";
-        int temp = 0; 
+        int temp = 0;
         if (isSyntacticallyCorrect(d))
             cout << "true";
         else
@@ -25,47 +27,82 @@ for (;;)
     }
 }
 
-bool isSyntacticallyCorrect(string dance){ 
-    char prev = '/';
-    int a = 0;
-    bool isDoubleDigit = false; 
-    for (int i = 1; i < static_cast<int>(dance.size()); i++)
+bool isSyntacticallyCorrect(string dance)
+{
+    int length = static_cast<int>(dance.size());
+    string temp = "";
+    bool containsSlash = false; 
+    for (int i = 0; i < length; i++)
     {
-        //check if char is valid first 
-        if (!(isValidDir(dance[i]) || dance[i] == '/' || isdigit(dance[i])))
+        //cout << "Temporary: " << temp << endl;
+        if (dance[i] == '/')
+        {
+            containsSlash = true; 
+            if (!isValidBeat(temp))
+            {
+
+                return false;
+            }
+            temp = "";
+        }
+        else
+        {
+            temp += dance[i];
+        }
+    }
+    //two edge cases are if dance has no '/' or if last char is no '/'
+    if(!containsSlash || dance[length-1] != '/'){ 
+        return false; 
+    }
+        return true;
+}
+
+bool isValidBeat(string s)
+{
+    int length = static_cast<int>(s.size());
+    if (s == "")
+    {
+        //cout << "Bypassed " << endl;
+        return true;
+    }
+    if (length > 3)
+    {
+        return false;
+    }
+    else if (length == 3)
+    {
+        if (isdigit(s[0]) && isdigit(s[1]) && isValidDir(s[2]))
+        {
+            ;
+        }
+        else
         {
             return false;
         }
-        
-        //check for valid dir after doubleDIgit flaged
-        if(isDoubleDigit && !(isValidDir(dance[i]))){
-            return false; 
-        }else{
-            isDoubleDigit = false; 
-        }
-        //if current char is '/' then prev must be / or dir 
-        if(dance[i] == '/'){ 
-            if(!(isValidDir(prev) || prev == '/')){
-                return false; 
-            }
-        }
-        
-        if(isalpha(dance[i]) && isalpha(prev)){ 
-            return false; 
-        }
-
-        //flag double digit if prev and current are digits
-        if(isdigit(dance[i])){ 
-            if(isdigit(prev)){ 
-                isDoubleDigit = true; 
-            } else if(isalpha(prev)){
-                return false;
-            }
-        }
-        a = i - 1;
-        prev = dance[a]; 
     }
-    return true; 
+    else if (length == 2)
+    {
+        if (isdigit(s[0]) && isValidDir(s[1]))
+        {
+            ;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        if (isValidDir(s[0]))
+        {
+            ;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 bool isValidDir(char c)
